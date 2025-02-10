9c755a30e7207c468fea4025867768a9f223dc2d
adityaoberai <adityaoberai1@gmail.com>
2025-02-11 03:35:37 +0530

    CSS improvements

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	const { data } = $props();
-	const { receiverName, senderName, sonnet } = data.message.message;
+	const { receiverName, sonnet } = data.message.message;
 </script>
 
 <svelte:head>
@@ -13,51 +13,5 @@
 	<div class="sonnet">
 		<p>{sonnet}</p>
 	</div>
-	<a href="https://lovesonnet.online" target="_blank" class="cta-button"
-		>Send {senderName} a Valentine's Day sonnet</a
-	>
+	<a href="https://lovesonnet.online" target="_blank" class="button">Send your beloved a Valentine's Day sonnet</a>
 </div>
-
-<style>
-	.container {
-		max-width: 800px;
-		width: 100%;
-		background: #fff;
-		padding: 40px;
-		border-radius: 10px;
-		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-		margin: auto;
-	}
-
-	h1 {
-		color: #d63384;
-		font-size: 36px;
-		margin-bottom: 20px;
-	}
-
-	p {
-		font-size: 18px;
-		color: #333;
-		line-height: 1.8;
-		margin: 15px 0;
-	}
-
-	.sonnet {
-		font-style: italic;
-		color: #555;
-		font-size: 18px;
-		margin: 30px auto;
-		white-space: pre-line;
-	}
-
-	.cta-button {
-		display: inline-block;
-		text-decoration: none;
-		margin: 30px auto;
-		padding: 15px 20px;
-		background-color: #ff4081;
-		color: #ffffff;
-		font-size: 20px;
-		border-radius: 5px;
-	}
-</style>
